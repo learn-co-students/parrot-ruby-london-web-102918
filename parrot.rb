0d7b4490949176ad2f8763d1067367006fad8b45
Paul Clark <clark.paul123@gmail.com>
2018-10-14 21:28:09 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+  if !phrase
+      puts phrase
+    elsif phrase
+      puts phrase
+    return phrase
+  end
+end
+    
